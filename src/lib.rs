32d3cac10e1baa62ab7b5f3b375e6358bd85be07
Ben Ashford <ben@bcash.org>
2015-04-20 19:54:31 +0100

    Beginning project layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-#[test]
-fn it_works() {
+#![crate_type = "lib"]
+#![crate_name = "rs_es"]
+
+#[macro_use] extern crate log;
+extern crate hyper;
+
+struct Client {
+    http_client: hyper::Client
+}
+
+impl Client {
+    fn new() -> Client {
+        Client {
+            http_client: hyper::Client::new()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Client;
+
+    #[test]
+    fn it_works() {
+        let mut client = Client::new();
+
+    }
 }
