bda249562eefea9dfab4a4777555c4a353050480
Ben Ashford <benashford@users.noreply.github.com>
2015-05-21 17:13:59 +0000

    Include the build script itself, it would be helpful

--- build.rs
+++ build.rs
@@ -2,11 +2,14 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     println!("Generating Query DSL to: {}", out_dir);
 
     // Use Ruby to generate src/query.rs - should probably be written in Rust
     // itself to avoid an unnecessary dependency
-    Command::new("tools/generate_query_dsl.rb").arg(out_dir).status().unwrap();
+    match Command::new("tools/generate_query_dsl.rb").arg(out_dir).status() {
+        Ok(exit_status) => println!("Finished with status: {}", exit_status),
+        Err(err)        => panic!("Error: {}", err)
+    }
 }
