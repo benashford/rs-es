f653fe3bac27689fb497d3a4062bc19869a6e787
Ben Ashford <benashford@users.noreply.github.com>
2016-06-05 15:02:18 +0000

    Field mapping options

--- src/operations/indices/mapping.rs
+++ src/operations/indices/mapping.rs
@@ -24,6 +24,7 @@ use serde::{Serialize, Serializer};
 
 use ::{Client, EsResponse};
 use ::error::EsError;
+use ::json::ShouldSkip;
 use ::operations::{format_multi, GenericResult};
 
 pub enum FieldType {
@@ -66,6 +67,12 @@ pub enum FieldType {
     Murmur3
 }
 
+impl Default for FieldType {
+    fn default() -> FieldType {
+        FieldType::String
+    }
+}
+
 impl Serialize for FieldType {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
         where S: Serializer {
@@ -91,16 +98,163 @@ impl Serialize for FieldType {
     }
 }
 
-#[derive(Serialize)]
+pub enum Dynamic {
+    True,
+    False,
+    Strict
+}
+
+impl From<bool> for Dynamic {
+    fn from(from: bool) -> Dynamic {
+        match from {
+            true => Dynamic::True,
+            false => Dynamic::False
+        }
+    }
+}
+
+impl Serialize for Dynamic {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: Serializer {
+        match self {
+            &Dynamic::True => true.serialize(serializer),
+            &Dynamic::False => false.serialize(serializer),
+            &Dynamic::Strict => "strict".serialize(serializer)
+        }
+    }
+}
+
+pub enum Index {
+    No,
+    NotAnalyzed,
+    Analyzed
+}
+
+impl Serialize for Index {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: Serializer {
+        use self::Index::*;
+        match self {
+            &No => "no",
+            &NotAnalyzed => "not_analyzed",
+            &Analyzed => "analyzed"
+        }.serialize(serializer)
+    }
+}
+
+#[derive(Default, Serialize)]
 pub struct Field {
     #[serde(rename="type")]
-    field_type: FieldType
+    field_type: FieldType,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // analyzer: Option<String>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    boost: Option<f64>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    coerce: Option<bool>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    copy_to: Option<String>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    doc_values: Option<bool>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    dynamic: Option<Dynamic>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    enabled: Option<bool>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // fielddata: Option<FieldData>,
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    geohash: Option<bool>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // geohash_precision: Option<GeohashPrecision>, // TODO - compare with geohash queries
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    geohash_prefix: Option<bool>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // format: Option<Format>
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    ignore_above: Option<u64>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    ignore_malformed: Option<bool>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    include_in_all: Option<bool>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // index_options: Option<IndexOptions>,
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    lat_lon: Option<bool>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    index: Option<Index>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // fields: Option<Fields>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // norms: Option<Norms>,
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    null_value: Option<String>,
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    position_incremental_gap: Option<u64>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // properties: Option<TypeProperties>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // search_analyzer: Option<SearchAnalyzer>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // similarity: Option<Similarity>
+
+    #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    store: Option<bool>,
+
+    // TODO - enable me
+    // #[serde(skip_serializing_if="ShouldSkip::should_skip")]
+    // term_vector: Option<TermVector>
+}
+
+impl Field {
+    add_field!(with_boost, boost, f64);
+    add_field!(with_coerce, coerce, bool);
+    add_field!(with_copy_to, copy_to, String);
+    add_field!(with_doc_values, doc_values, bool);
+    add_field!(with_dynamic, dynamic, Dynamic);
+    add_field!(with_enabled, enabled, bool);
+    add_field!(with_geohash, geohash, bool);
+    add_field!(with_geohash_prefix, geohash_prefix, bool);
+    add_field!(with_ignore_above, ignore_above, u64);
+    add_field!(with_ignore_malformed, ignore_malformed, bool);
+    add_field!(with_include_in_all, include_in_all, bool);
+    add_field!(with_lat_lon, lat_lon, bool);
+    add_field!(with_index, index, Index);
+    add_field!(with_null_value, null_value, String);
+    add_field!(with_position_incremental_gap, position_incremental_gap, u64);
+//    add_field!(with_properties, properties, TypeProperties);
+    add_field!(with_store, store, bool);
 }
 
 impl From<FieldType> for Field {
     fn from(from: FieldType) -> Field {
         Field {
-            field_type: from
+            field_type: from,
+            ..Default::default()
         }
     }
 }
